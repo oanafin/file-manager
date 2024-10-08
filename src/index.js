ec4428097fd1384f712cce71c88903165bc2ebb2
oanafin <olzhas.anafin@gmail.com>
2024-10-08 23:11:48 +0500

    feat: add readline, close

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+import readline from 'readline';
+
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
 const username = usernameArg ? usernameArg.split('=')[1] : 'User';
 console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    //
+});
+
+rl.on('close', () => {
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+});
