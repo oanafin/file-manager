ab920bf0babe09d84293926835f605025d84a4eb
oanafin <olzhas.anafin@gmail.com>
2024-10-08 22:48:48 +0500

    init: start file-manager project

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const args = process.argv.slice(2);
+const usernameArg = args.find(arg => arg.startsWith('--username='));
+const username = usernameArg ? usernameArg.split('=')[1] : 'User';
+console.log(`Welcome to the File Manager, ${username}!`);
